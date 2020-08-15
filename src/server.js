fd1f9271cf9fc4a1edfd428e77a8b13e4554a7b0
jin <s3736719@student.rmit.edu.au>
2020-08-15 19:57:49 +1000

    test

--- src/server.js
+++ src/server.js
@@ -13,10 +13,19 @@ const todos = require("./api/todos");
 app.use("/api/todos", todos);
 
 // Static web app
-app.use(express.static(path.join(__dirname, "/../client/build")));
-app.get("*", (req, res) => {
-  res.sendFile(path.join(__dirname + "/../client/build/index.html"));
-});
+if (
+  process.env.NODE_ENV === "production" ||
+  process.env.NODE_ENV === "staging"
+) {
+  app.use(express.static("client/build"));
+  app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname + "/client/build/index.html"));
+  });
+}
+// app.use(express.static(path.join(__dirname, "/../client/build")));
+// app.get("*", (req, res) => {
+//   res.sendFile(path.join(__dirname + "/../client/build/index.html"));
+// });
 
 const port = process.env.PORT || 5000;
 app.listen(port, () => {
